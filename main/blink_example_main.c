10bde52bdcea5ca97f238c9f30e921ec76e59989
erickchicatto1 <robo.12@outlook.es>
2025-01-22 17:55:34 -0600

    ESP32 write in the cmd

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -7,28 +7,28 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <string.h>
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 void app_main(void)
 {
+    //Define a char
+    char c = 0 ; // this char is going to be used with the getchar function
+    //Define an array with chars
+    char str[100];
+    //Fill the array with ceros
+    memset(str,0,sizeof(str));
 
-//1. Declare array to store what we write in the cmd
-char data;//Data in where we are going to save the data user
-char str[100];//array to store the characters
-memset(str,0,sizeof(str));//to start all the elements with cero
+    while(c != '\n'){ //This is for check that we don't have line break
+       c = getchar();//get the char
 
-//2. Check if we don't use \n
-if(str != '\n'){ //Check for the line break
-   data = getchar();//Get the data from the cmd
-   str[100]=data // don't understand
-   
-}
-//3. Check if we don't write 0xff
-
-//4. Convert the values in characters
-
-//5.   
-  
+       if(c!=0xff){ //check if the char is different to 0xff
+        str[strlen(str)] = c; // to avoid the void char
+        printf("%c",c);
+       }
+      vTaskDelay(100/portTICK_PERIOD_MS);
+    }
+    printf("you typed : %s\n",str);
 }
