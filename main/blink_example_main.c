0cd2561ea730a7dfb1f72b4945e6d2a4c602fa3b
erickchicatto1 <robo.12@outlook.es>
2025-01-22 15:01:13 -0600

    Esp32 Random Number
    In this example I understand how to create a random numbers
    and use the delay function from FreeRtos

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -11,15 +11,35 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#define TAG   "DELAY"
+//Add libraries 
+#include "esp_random.h"
+#include "math.h"
+
+#define TAG   "DICE"
+
+//Function to ensure that only we are going to have six faces to random numbers
+int numberDice(){
+    //1.Get the number random
+    int randomNumber = esp_random();
+    //Need to be positive
+    int positiveNumber = abs(randomNumber);
+    //2. Ensure that only its going to be into 6 range
+    int ensureNumber = (positiveNumber % 6) + 1;
+    //3.return the number
+    return ensureNumber;
+}
+
+
 
 void app_main(void)
 {
-    int i=0;
-    
-    while(1){
-      vTaskDelay(1000/portTICK_PERIOD_MS);
-      ESP_LOGI(TAG,"\n Counting %d ",i++);
-    }
+  //1.Make a delay
+  while(1){
+     vTaskDelay(1000/portTICK_PERIOD_MS);
+     //2.Ensure that delay be an infinite loop 
+     //3.print into the log the value of the random number beetween 1 - 6
+     int espNumberRandom = numberDice();
+     ESP_LOGI(TAG,"This is the random number : %d",espNumberRandom);
+  }
  
 }
