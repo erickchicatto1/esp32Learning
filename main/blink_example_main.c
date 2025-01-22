4d304a9116bfcbadb7bb6b5131020f3cf683bb23
erickchicatto1 <robo.12@outlook.es>
2025-01-22 17:26:22 -0600

    ESP32 My own approach

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -10,40 +10,25 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-//1. Add the library for the gpios
-#include "driver/gpio.h"
-
-#define TAG   "BLINK"
-// LL define the pin 
-#define PINR  0   // Red
-#define PING  2   // Green
-#define PINB 4  // Blue
 
 void app_main(void)
 {
 
- //Config if the gpio is input or output
- //gpio_set_direction(GPIO_NUM_0,GPIO_MODE_DEF_OUTPUT); my error
-  gpio_set_direction(PINR,GPIO_MODE_OUTPUT); 
-  gpio_set_direction(PING,GPIO_MODE_OUTPUT);
-  gpio_set_direction(PINB,GPIO_MODE_OUTPUT);
+//1. Declare array to store what we write in the cmd
+char data;//Data in where we are going to save the data user
+char str[100];//array to store the characters
+memset(str,0,sizeof(str));//to start all the elements with cero
 
-  uint32_t isOn = 0 ;  
+//2. Check if we don't use \n
+if(str != '\n'){ //Check for the line break
+   data = getchar();//Get the data from the cmd
+   str[100]=data // don't understand
+   
+}
+//3. Check if we don't write 0xff
 
-  while(1){
-    //Create variable flag to on and off
-    isOn = !isOn;
-    //uint8_t LedOn=0; my error
-    //2.Set the level of the gpio
-    gpio_set_level(PINR,isOn);
-    //LedOn = !LedOn;my error
-    //3.make a infinite loop to on and off the led every 1 second
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-    gpio_set_level(PING,isOn);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-    gpio_set_level(PINB,isOn);
-    
-  }
+//4. Convert the values in characters
 
+//5.   
   
 }
