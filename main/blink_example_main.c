244740946243338c69b5f14d49148df3b4870c56
erickchicatto1 <robo.12@outlook.es>
2025-02-17 10:22:43 -0600

    Study Task In FreeRtos two functions

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -6,29 +6,52 @@
 
 static TaskHandle_t receiverHandler = NULL;
 
-void sender(void *params){
 
+void sender(void *params){
     while(true){
         xTaskNotifyGive(receiverHandler);
         vTaskDelay(5000/portTICK_PERIOD_MS);
     }
-
 }
 
 void receiver(void *params){
-   
-   while(true){
-    ulTaskNotifyTake(pdFALSE,portMAX_DELAY);
-    printf("Received notification \n");
-   }
-
-
+    while(true){
+        ulTaskNotifyTake(pdFALSE,portMAX_DELAY);
+        printf("Receive notification \n");
+    }
 }
 
 
 
-//2. Define another task for FreeRtos 
+
 void app_main(void){
-   xTaskCreate(&receiver, "sender", 2048,NULL, 2,&receiverHandler);
-   xTaskCreate(&sender, "receiver", 2048, NULL, 2, NULL);
+    
+    //Define a char
+    char c = 0;
+
+    //Define an array with chars
+    char str[100];
+
+    //Fill the array with zeros
+    memset(str,0,sizeof(str));
+
+    //check that we dont have a line brake
+    while(c != '\n'){
+      
+      c = getchar(); //get the char
+
+        if(c!=0xff){
+            str[strlen(str)] = c; // eg. hola -> str[4]= c -> holac, to add characters
+            printf("%c",c);
+        }
+        vTaskDelay(100/portTICK_PERIOD_MS);
+    }
+
+    printf("you typed : %s \n",str);
+
+
+    //Create the task.
+    xTaskCreate(&receiver,"sender",2048,NULL,2,&receiverHandler);
+    xTaskCreate(&sender, "receiver", 2048, NULL, 2, NULL);
+
 }
